ad7ed2dbf87432289b748d12b3a79d7fd9c4bb78
Alex <alexanliakou@gmail.com>
2026-06-28 14:15:41 +0300

    added some fixes

--- src/pages/CharacterDetailsPage.tsx
+++ src/pages/CharacterDetailsPage.tsx
@@ -32,9 +32,9 @@ const CharacterDetailsPage = () => {
                 }}
             />
         </div>
-        <div className='border border-star-mustard rounded-md p-8 my-10'>
-            <div className='grow flex flex-col gap-4 items-center justify-center'>
-                <h2 className="text-star-creme text-xl md:text-3xl font-bold text-center ">Personal Information</h2>
+        <div className='border border-star-mustard rounded-md p-8 my-10 grid grid-cols-1 md:grid-cols-2'>
+            <div className='grow flex flex-col gap-4 items-center justify-center mb-10'>
+                <h2 className="text-star-creme text-xl md:text-3xl font-bold text-center">Personal Information</h2>
                 <DetailBlock label={'Gender'} value={character?.gender}/>
                 <DetailBlock label={'Homeworld'} value={relations.homeworld.isLoading ? 'Loading...' : relations.homeworld.isError ? 'Failed to load' : relations.homeworld.data?.name ?? 'Unknown'}/>
                 <DetailBlock label="Species" value={relations.species.isLoading ? 'Loading...' : relations.species.isError ? 'Failed to load' : relations.species.data.map(sp => sp.name).join(', ') || 'Unknown'} />
@@ -43,53 +43,59 @@ const CharacterDetailsPage = () => {
                 <DetailBlock label={'Weight'} value={`${character?.mass} kg`}/>
                 <DetailBlock label={'Eye Color'} value={character?.eye_color}/>
                 <DetailBlock label={'Hair Color'} value={character?.hair_color}/>
-                <DetailBlock label={'Skin Color'} value={character?.skin_color}/>
+                <DetailBlock label={'Skin Color'} value={character?.skin_color}/> 
             </div>
-            <h2 className="text-star-creme text-xl md:text-3xl font-bold text-center mt-10 mb-5">Films</h2>
-            <div className='grow flex flex-col gap-4 items-center justify-center'>
-                    {relations.films.isLoading ? (
-                        <p className="text-star-yellow text-xl font-bold italic mt-5">Loading...</p> 
-                    ) : relations.films.isError ? (
-                        <p className="text-star-yellow text-xl font-bold italic mt-5">Failed to load films</p>
-                    ) : relations.films.data.length > 0 ? (                       
-                            relations.films.data.sort((a, b) => a.episode_id - b.episode_id)
-                            .map(f => (
-                            <Link className='text-star-green cursor-pointer hover:text-star-mustard' key={f.url} to={`/films/${idFromUrl(f.url)}`}>{f.title}</Link>
-                            ))
-                        )
-                        :
-                        <p className="text-star-yellow text-xl font-bold italic mt-5">No films found</p>      
+            <div className='mb-10'>
+                <h2 className="text-star-creme text-xl md:text-3xl font-bold text-center mb-4">Films</h2>
+                <div className='grow flex flex-col gap-4 items-center justify-center'>
+                        {relations.films.isLoading ? (
+                            <p className="text-star-yellow text-xl font-bold italic mt-5">Loading...</p> 
+                        ) : relations.films.isError ? (
+                            <p className="text-star-yellow text-xl font-bold italic mt-5">Failed to load films</p>
+                        ) : relations.films.data.length > 0 ? (                       
+                                relations.films.data.sort((a, b) => a.episode_id - b.episode_id)
+                                .map(f => (
+                                <Link className='text-star-green cursor-pointer hover:text-star-mustard' key={f.url} to={`/films/${idFromUrl(f.url)}`}>{f.title}</Link>
+                                ))
+                            )
+                            :
+                            <p className="text-star-yellow text-xl font-bold italic mt-5">No films found</p>      
+                        }
+                </div>
+            </div>
+            <div className='mb-10'>
+                <h2 className="text-star-creme text-xl md:text-3xl font-bold text-center mb-4">Starships</h2>
+                <div className='grow flex flex-col gap-4 items-center justify-center'>
+                    {relations.starships.isLoading ? (
+                            <p className="text-star-yellow text-xl font-bold italic mt-5">Loading...</p>
+                        ) : relations.starships.isError ? (
+                            <p className="text-star-yellow text-xl font-bold italic mt-5">Failed to load starships</p>
+                        ) : relations.starships.data.length > 0 ? (    
+                                relations.starships.data.map(s => (
+                                    <p className='text-star-green' key={s.url}><span className='text-star-blue'>{s.name}</span> - {s.model}</p>
+                                ))                                                                                                        
+                            )
+                            :
+                            <p className="text-star-yellow text-xl font-bold italic mt-5">No starships found</p> 
                     }
+                </div>
             </div>
-            <h2 className="text-star-creme text-xl md:text-3xl font-bold text-center mt-10 mb-5">Starships</h2>
-            <div className='grow flex flex-col gap-4 items-center justify-center'>
-                {relations.starships.isLoading ? (
-                        <p className="text-star-yellow text-xl font-bold italic mt-5">Loading...</p>
-                    ) : relations.starships.isError ? (
-                        <p className="text-star-yellow text-xl font-bold italic mt-5">Failed to load starships</p>
-                    ) : relations.starships.data.length > 0 ? (    
-                            relations.starships.data.map(s => (
-                                <p className='text-star-green' key={s.url}><span className='text-star-blue'>{s.name}</span> - {s.model}</p>
-                            ))                                                                                                        
-                        )
-                        :
-                        <p className="text-star-yellow text-xl font-bold italic mt-5">No starships found</p> 
-                }
-            </div>
-            <h2 className="text-star-creme text-xl md:text-3xl font-bold text-center mt-10 mb-5">Vehicles</h2>
-            <div className='grow flex flex-col gap-4 items-center justify-center'>
-                {relations.vehicles.isLoading ? (
-                        <p className="text-star-yellow text-xl font-bold italic mt-5">Loading...</p>
-                    ) : relations.vehicles.isError ? (
-                        <p className="text-star-yellow text-xl font-bold italic mt-5">Failed to load vehicles</p>
-                    ) : relations.vehicles.data.length > 0 ? (     
-                            relations.vehicles.data.map(v => (
-                                <p className='text-star-green' key={v.url}><span className='text-star-blue'>{v.name}</span> - {v.model}</p>
-                            ))                           
-                        )
-                        :
-                        <p className="text-star-yellow text-xl font-bold italic mt-5">No starships found</p> 
-                }
+            <div className='mb-10'>
+                <h2 className="text-star-creme text-xl md:text-3xl font-bold text-center mb-4">Vehicles</h2>
+                <div className='grow flex flex-col gap-4 items-center justify-center'>
+                    {relations.vehicles.isLoading ? (
+                            <p className="text-star-yellow text-xl font-bold italic mt-5">Loading...</p>
+                        ) : relations.vehicles.isError ? (
+                            <p className="text-star-yellow text-xl font-bold i className='mb-4'talic mt-5">Failed to load vehicles</p>
+                        ) : relations.vehicles.data.length > 0 ? (     
+                                relations.vehicles.data.map(v => (
+                                    <p className='text-star-green' key={v.url}><span className='text-star-blue'>{v.name}</span> - {v.model}</p>
+                                ))                           
+                            )
+                            :
+                            <p className="text-star-yellow text-xl font-bold italic mt-5">No starships found</p> 
+                    }
+                </div>
             </div>
         </div>
         <Link className='rounded-md p-5 !bg-star-creme text-black font-bold cursor-pointer hover:!bg-star-yellow' to="/characters">← Back to Characters</Link>  
--- src/pages/FilmDetailsPage.tsx
+++ src/pages/FilmDetailsPage.tsx
@@ -32,95 +32,107 @@ const FilmDetailsPage = () => {
                 }}
             />
         </div>
-        <div className='border border-star-mustard rounded-md p-8 my-10'>
-            <div className='grow flex flex-col gap-4 items-center justify-center'>
-                <h2 className="text-star-creme text-xl md:text-3xl font-bold text-center ">General Information</h2>
+        <div className='border border-star-mustard rounded-md p-8 my-10 grid grid-cols-1 md:grid-cols-2'>
+            <div className='grow flex flex-col gap-4 items-center mb-10'>
+                <h2 className="text-star-creme text-xl md:text-3xl font-bold text-center mt-10">General Information</h2>
                 <DetailBlock label={'Released'} value={new Date(film!.release_date).getFullYear()}/>
                 <DetailBlock label={'Director'} value={film?.director}/>
                 <DetailBlock label={'Producer/s'} value={film?.producer}/>
             </div>
-            <h2 className="text-star-creme text-xl md:text-3xl font-bold text-center mt-10 mb-5">Introduction</h2>
-            <div className='flex items-center justify-center'><p style={{maxWidth: 500}} className='text-star-green text-lg'>{film?.opening_crawl}</p></div>
-            <h2 className="text-star-creme text-xl md:text-3xl font-bold text-center mt-10 mb-5">Characters</h2>
-            <div className='grow flex flex-col gap-4 items-center justify-center'>
-                {relations.characters.isLoading ? (
-                    <p className="text-star-yellow text-xl font-bold italic mt-5">Loading...</p> 
-                    ) : relations.characters.isError ? (
-                    <p className="text-star-yellow text-xl font-bold italic mt-5">Failed to load characters</p>
-                    ) : relations.characters.data.length > 0 ?
-                        (
-                        relations.characters.data
-                            .map(c => (
-                                <Link className='text-star-green cursor-pointer hover:text-star-mustard' key={c.url} to={`/characters/${idFromUrl(c.url)}`}>
-                                    {c.name}
-                                </Link>
-                            ))
-                )
-                :
-                <p className="text-star-yellow text-xl font-bold italic mt-5">No characters found</p>        
-            }
+            <div className='mb-10'>
+                <h2 className="text-star-creme text-xl md:text-3xl font-bold text-center mt-10 mb-5">Introduction</h2>
+                <div className='flex items-center justify-center'><p style={{maxWidth: 500}} className='text-star-green text-lg'>{film?.opening_crawl}</p></div>
             </div>
-            <h2 className="text-star-creme text-xl md:text-3xl font-bold text-center mt-10 mb-5">Planets</h2>
-            <div className='grow flex flex-col gap-4 items-center justify-center'>
-                {relations.planets.isLoading ? (
-                    <p className="text-star-yellow text-xl font-bold italic mt-5">Loading...</p> 
-                ) : relations.planets.isError ? (
-                    <p className="text-star-yellow text-xl font-bold italic mt-5">Failed to load planets</p>
-                ) : relations.planets.data.length > 0 ?
-                (
-                    relations.planets.data.map(p => (
-                        <p key={p.url} className='text-star-green'>{p.name}</p>
-                    ))
-                )
-                :
-                <p className="text-star-yellow text-xl font-bold italic mt-5">No planets found</p>        
-            }
+            <div className='mb-10'>
+                <h2 className="text-star-creme text-xl md:text-3xl font-bold text-center mt-10 mb-5">Vehicles</h2>
+                <div className='grow flex flex-col gap-4 items-center justify-center'>
+                    {relations.vehicles.isLoading ? (
+                            <p className="text-star-yellow text-xl font-bold italic mt-5">Loading...</p> 
+                        ) : relations.vehicles.isError ? (
+                            <p className="text-star-yellow text-xl font-bold italic mt-5">Failed to load vehicles</p>
+                        ) : relations.vehicles.data.length > 0 ?(                           
+                                relations.vehicles.data.map(v => (
+                                    <p className='text-star-green' key={v.url}><span className='text-star-blue'>{v.name}</span> - {v.model}</p>
+                                ))
+                        )
+                        :
+                        <p className="text-star-yellow text-xl font-bold italic mt-5">No vehicles found</p> 
+                    }            
+                </div>
             </div>
-            <h2 className="text-star-creme text-xl md:text-3xl font-bold text-center mt-10 mb-5">Species</h2>
-            <div className='grow flex flex-col gap-4 items-center justify-center'>
-                {relations.species.isLoading ? (
+            <div className='mb-10'>
+                <h2 className="text-star-creme text-xl md:text-3xl font-bold text-center mt-10 mb-5">Starships</h2>
+                <div className='grow flex flex-col gap-4 items-center justify-center'>
+                    {relations.starships.isLoading ? (
+                            <p className="text-star-yellow text-xl font-bold italic mt-5">Loading...</p> 
+                        ) : relations.starships.isError ? (
+                            <p className="text-star-yellow text-xl font-bold italic mt-5">Failed to load starships</p>
+                        ) : relations.starships.data.length > 0 ? (                       
+                                relations.starships.data.map(s => (
+                                    <p className='text-star-green' key={s.url}><span className='text-star-blue'>{s.name}</span> - {s.model}</p>
+                                ))                           
+                        )
+                        :
+                        <p className="text-star-yellow text-xl font-bold italic mt-5">No planets found</p>        
+                    }            
+                </div>
+            </div>
+            <div className='mb-10'>
+                <h2 className="text-star-creme text-xl md:text-3xl font-bold text-center mt-10 mb-5">Characters</h2>
+                <div className='grow flex flex-col gap-4 items-center justify-center'>
+                    {relations.characters.isLoading ? (
                         <p className="text-star-yellow text-xl font-bold italic mt-5">Loading...</p> 
-                    ) : relations.species.isError ? (
-                        <p className="text-star-yellow text-xl font-bold italic mt-5">Failed to load species</p>
-                    ) :  relations.species.data.length > 0 ? ( 
-                            relations.species.data.map(s => (
-                                <p className='text-star-green' key={s.url}>{s.name}</p>
-                            ))
+                        ) : relations.characters.isError ? (
+                        <p className="text-star-yellow text-xl font-bold italic mt-5">Failed to load characters</p>
+                        ) : relations.characters.data.length > 0 ?
+                            (
+                            relations.characters.data
+                                .map(c => (
+                                    <Link className='text-star-green cursor-pointer hover:text-star-mustard' key={c.url} to={`/characters/${idFromUrl(c.url)}`}>
+                                        {c.name}
+                                    </Link>
+                                ))
+                    )
+                    :
+                    <p className="text-star-yellow text-xl font-bold italic mt-5">No characters found</p>        
+                }
+                </div>
+            </div>
+            <div className='mb-10'>
+            <h2 className="text-star-creme text-xl md:text-3xl font-bold text-center mt-10 mb-5">Species</h2>
+                <div className='grow flex flex-col gap-4 items-center justify-center'>
+                    {relations.species.isLoading ? (
+                            <p className="text-star-yellow text-xl font-bold italic mt-5">Loading...</p> 
+                        ) : relations.species.isError ? (
+                            <p className="text-star-yellow text-xl font-bold italic mt-5">Failed to load species</p>
+                        ) :  relations.species.data.length > 0 ? ( 
+                                relations.species.data.map(s => (
+                                    <p className='text-star-green' key={s.url}>{s.name}</p>
+                                ))
+                        )
+                        :
+                        <p className="text-star-yellow text-xl font-bold italic mt-5">No planets found</p>        
+                    }
+                </div>
+            </div>
+            <div className='mb-10'>
+                <h2 className="text-star-creme text-xl md:text-3xl font-bold text-center mt-10 mb-5">Planets</h2>
+                <div className='grow flex flex-col gap-4 items-center justify-center'>
+                    {relations.planets.isLoading ? (
+                        <p className="text-star-yellow text-xl font-bold italic mt-5">Loading...</p> 
+                    ) : relations.planets.isError ? (
+                        <p className="text-star-yellow text-xl font-bold italic mt-5">Failed to load planets</p>
+                    ) : relations.planets.data.length > 0 ?
+                    (
+                        relations.planets.data.map(p => (
+                            <p key={p.url} className='text-star-green'>{p.name}</p>
+                        ))
                     )
                     :
                     <p className="text-star-yellow text-xl font-bold italic mt-5">No planets found</p>        
                 }
+                </div>
             </div>
-            {/* <h2 className="text-star-creme text-xl md:text-3xl font-bold text-center mt-10 mb-5">Starships</h2>
-            <div className='grow flex flex-col gap-4 items-center justify-center'>
-                {relations.starships.isLoading ? (
-                        <p className="text-star-yellow text-xl font-bold italic mt-5">Loading...</p> 
-                    ) : relations.starships.isError ? (
-                        <p className="text-star-yellow text-xl font-bold italic mt-5">Failed to load starships</p>
-                    ) : relations.starships.data.length > 0 ? (                       
-                            relations.starships.data.map(s => (
-                                <p className='text-star-green' key={s.url}><span className='text-star-blue'>{s.name}</span> - {s.model}</p>
-                            ))                           
-                    )
-                    :
-                    <p className="text-star-yellow text-xl font-bold italic mt-5">No planets found</p>        
-                }            
-            </div> */}
-            {/* <h2 className="text-star-creme text-xl md:text-3xl font-bold text-center mt-10 mb-5">Vehicles</h2>
-            <div className='grow flex flex-col gap-4 items-center justify-center'>
-                {relations.vehicles.isLoading ? (
-                        <p className="text-star-yellow text-xl font-bold italic mt-5">Loading...</p> 
-                    ) : relations.vehicles.isError ? (
-                        <p className="text-star-yellow text-xl font-bold italic mt-5">Failed to load vehicles</p>
-                    ) : relations.vehicles.data.length > 0 ?(                           
-                            relations.vehicles.data.map(v => (
-                                <p className='text-star-green' key={v.url}><span className='text-star-blue'>{v.name}</span> - {v.model}</p>
-                            ))
-                    )
-                    :
-                    <p className="text-star-yellow text-xl font-bold italic mt-5">No vehicles found</p> 
-                }            
-            </div> */}
         </div>
         <Link className='rounded-md p-5 !bg-star-creme text-black font-bold cursor-pointer hover:!bg-star-yellow' to="/films">← Back to Films</Link>       
     </section>
